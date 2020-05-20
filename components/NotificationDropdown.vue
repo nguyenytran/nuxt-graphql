5b313ffe98b257e195e0d10f23d3b00249cb53e3
ytran <ytran@dosiinvn.com>
2020-05-20 17:05:11 +0700

    init project

---
+++ components/NotificationDropdown.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <a
+      ref="btnDropdownRef"
+      class="text-gray-600 block py-1 px-3"
+      href="#pablo"
+      @click="toggleDropdown($event)"
+    >
+      <i class="fas fa-bell"></i>
+    </a>
+    <div
+      ref="popoverDropdownRef"
+      class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1"
+      :class="{
+        hidden: !dropdownPopoverShow,
+        block: dropdownPopoverShow
+      }"
+      style="min-width:12rem"
+    >
+      <a
+        href="#pablo"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Action
+      </a>
+      <a
+        href="#pablo"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Another action
+      </a>
+      <a
+        href="#pablo"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Something else here
+      </a>
+      <div class="h-0 my-2 border border-solid border-gray-200" />
+      <a
+        href="#pablo"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Seprated link
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+import Popper from 'popper.js'
+
+export default {
+  data() {
+    return {
+      dropdownPopoverShow: false
+    }
+  },
+  methods: {
+    toggleDropdown(event) {
+      event.preventDefault()
+      if (this.dropdownPopoverShow) {
+        this.dropdownPopoverShow = false
+      } else {
+        this.dropdownPopoverShow = true
+        // eslint-disable-next-line no-new
+        new Popper(this.$refs.btnDropdownRef, this.$refs.popoverDropdownRef, {
+          placement: 'bottom-end'
+        })
+      }
+    }
+  }
+}
+</script>
---
+++ plugins/apollo-error-handler.js
@@ -0,0 +1,4 @@
+export default (
+  { graphQLErrors, networkError, operation, forward },
+  nuxtContext
+) => {}
